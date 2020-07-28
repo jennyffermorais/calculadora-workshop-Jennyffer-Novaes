ea9dc39af0490a65c0c2c6facc1eea87e3b6ed41
Jennyffer de Morais <jennyffernovaes@hotmail.com>
2020-07-28 14:30:54 -0300

    adicionar resolucao do desafio

---
+++ js/desafio.js
@@ -0,0 +1,28 @@
+var inputValorHora = document.querySelector("#valor-hora");
+
+var inputHorasProjeto = document.querySelector("#horas-projeto");
+
+var resposta = document.querySelector("#resposta");
+
+inputValorHora.addEventListener("input", apenasNumeros);
+
+inputHorasProjeto.addEventListener("input", apenasNumeros);
+
+function calcular() {
+  // pegar valores
+  var valorHora = inputValorHora.valueAsNumber;
+  var horasProjeto = inputHorasProjeto.valueAsNumber;
+
+  // calcular valor total do projeto
+  var valorTotal = valorHora * horasProjeto;
+  var valorDuasCasas = valorTotal.toFixed(2).replace(".", ",");
+  // console.log(valorDuasCasas)
+
+  resposta.textContent = "O valor total do projeto é R$ " + valorDuasCasas;
+}
+
+// remover o que não for número e substituir por vazio
+function apenasNumeros(event) {
+  var valor = event.currentTarget.value;
+  event.currentTarget.value = valor.replace(/[^0-9]/g, "");
+}
